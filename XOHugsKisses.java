a8fc24266f0deaf1272ca18a252eaf23c0e82362
Adeeb Sheikh <asheikh4@u.rochester.edu>
2016-02-07 20:10:31 -0500

    Scrapped some stuff while I figure out how to approach this.

--- XOHugsKisses.java
+++ XOHugsKisses.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class XOHugsKisses {
 
-	int[][] board = new int[3][3];
+	double[] minimaxHeap;
 	Scanner scanner = new Scanner(System.in);
 	//true if I am x, false if I am o
 	boolean xo;
@@ -12,15 +12,13 @@ public class XOHugsKisses {
 		System.err.println("Will you play X or O?");
 		String input = scanner.next();
 		input = input.toLowerCase();
+		
 		if(input.equals("o"))
 			xo = true;
 		else if(input.equals("x"))
 			xo = false;
 		else
 			System.err.println("You're a dumbass.");	
-		
-		if(xo);//TO-DO: Make first move
-		else;//TO-DO: Let user make first move, play game
 		
 	}
 	
