eab6efc3aec1d0ac3a9fef7ac5e091f2e56c4ea0
Adeeb Sheikh <asheikh4@u.rochester.edu>
2016-02-08 23:32:18 -0500

    XO is the simple tic-tac-toe player -- XOHugsKisses is Super Tic-Tac-Toe.

---
+++ XO.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class XO {
+
+	double[] minimaxHeap;
+	Scanner scanner = new Scanner(System.in);
+	//true if I am x, false if I am o
+	boolean xo;
+	
+	public XO(){
+		
+		System.err.println("Will you play X or O?");
+		String input = scanner.next();
+		input = input.toLowerCase();
+		
+		if(input.equals("o"))
+			xo = true;
+		else if(input.equals("x"))
+			xo = false;
+		else
+			System.err.println("You're a dumbass.");	
+		
+	}
+	
+}
