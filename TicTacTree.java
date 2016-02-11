4008bfdfa2063969fa45abd5bd149b0d75c6929f
Adeeb Sheikh <asheikh4@u.rochester.edu>
2016-02-11 16:39:47 -0500

    Minimax kinda works. :/

--- TicTacTree.java
+++ TicTacTree.java
@@ -1,4 +1,285 @@
 
 public class TicTacTree {
 
+	//Game-related
+	private int[][] board;
+	private int[] combos;
+	private boolean isMyMove;
+	private boolean goalState;
+	private int winner;
+	private int x;
+	private int y;
+	
+	//Tree-related
+	private TicTacTree parent;
+	private TicTacTree leftChild;
+	private TicTacTree rightSibling;
+	
+	int depth;
+	
+	public TicTacTree(){
+		
+		board = new int[3][3];
+		combos = new int[8];
+		isMyMove = false;
+		goalState = false;
+		winner = 0;
+		x = -1;
+		y = -1;
+		parent = null;
+		leftChild = null;
+		rightSibling = null;
+		depth = 0;
+		
+	}
+	
+	public TicTacTree(int move){
+		
+		board = new int[3][3];
+		combos = new int[8];
+		isMyMove = false;
+		goalState = false;
+		winner = 0;
+		x = (move - 1)%3;
+		y = (move - 1)/3;
+		board[x][y] = -1;
+		parent = null;
+		leftChild = null;
+		rightSibling = null;
+		depth = 0;
+		
+	}
+	
+	public TicTacTree(TicTacTree parent, int[][] oldBoard, int[] oldCombos, int move, boolean isMyMove){
+		depth = parent.depth + 1;
+		this.parent = parent;
+		leftChild = null;
+		rightSibling = null;
+		
+		goalState = false;
+		winner = 0;
+		this.isMyMove = isMyMove;
+		x = (move - 1)%3;
+		y = (move - 1)/3;
+		board = new int[oldBoard.length][oldBoard[0].length];
+		
+		//Transfer the board over -- plus the move that was made
+		int wiener = 1;
+		for(int i = 0; i < board.length; i++)
+			for(int j = 0; j < board[i].length; j++){
+				if(i != x || j != y)
+					board[i][j] = oldBoard[i][j];
+				else
+					if(isMyMove)
+						board[i][j] = 1;
+					else
+						board[i][j] = -1;
+				wiener *= board[i][j];
+			}
+		//If the board is full, this is a terminal state
+		if(wiener != 0)
+			goalState = true;
+		
+		//Check for winning combinations
+		//0, 1, 2 are the vertical combos
+		//3, 4, 5 are the horizontal combos
+		//6, 7 are the diagonal combos
+		combos = new int[oldCombos.length];
+		for(int i = 0; i < combos.length; i++)
+			combos[i] = oldCombos[i];
+		
+		combos[x] += board[x][y];
+		if(Math.abs(combos[x]) >= 3){
+			
+			goalState = true;
+			winner = Math.abs(combos[x])/combos[x];
+			
+		}
+		combos[y + 3] += board[x][y];
+		if(Math.abs(combos[y + 3]) >= 3){
+			
+			goalState = true;
+			winner = Math.abs(combos[y + 3])/combos[y + 3];
+			
+		}
+		if(x == y){
+			
+			combos[6] += board[x][y];
+			if(Math.abs(combos[6]) >= 3){
+				
+				goalState = true;
+				winner = Math.abs(combos[6])/combos[6];
+				
+			}
+			
+		}
+		if(x == 2 - y){
+			
+			combos[7] += board[x][y];
+			if(Math.abs(combos[7]) >= 3){
+				
+				goalState = true;
+				winner = Math.abs(combos[7])/combos[7];
+				
+			}
+			
+		}
+		
+	}
+	
+	public TicTacTree(TicTacTree parent, int[][] oldBoard, int[] oldCombos, int x, int y, boolean isMyMove){
+		depth = parent.depth + 1;
+		this.parent = parent;
+		leftChild = null;
+		rightSibling = null;
+		
+		goalState = false;
+		winner = 0;
+		this.isMyMove = isMyMove;
+		this.x = x;
+		this.y = y;
+		board = new int[oldBoard.length][oldBoard[0].length];
+		
+		//Transfer the board over -- plus the move that was made
+		int wiener = 1;
+		for(int i = 0; i < board.length; i++)
+			for(int j = 0; j < board[i].length; j++){
+				if(i != x || j != y)
+					board[i][j] = oldBoard[i][j];
+				else
+					if(isMyMove)
+						board[i][j] = 1;
+					else
+						board[i][j] = -1;
+				wiener *= board[i][j];
+			}
+		//If the board is full, this is a terminal state
+		if(wiener != 0)
+			goalState = true;
+		
+		//Check for winning combinations
+		combos = new int[oldCombos.length];
+		for(int i = 0; i < combos.length; i++)
+			combos[i] = oldCombos[i];
+		
+		combos[x] += board[x][y];
+		if(Math.abs(combos[x]) >= 3){
+			
+			goalState = true;
+			winner = Math.abs(combos[x])/combos[x];
+			
+		}
+		combos[y + 3] += board[x][y];
+		if(Math.abs(combos[y + 3]) >= 3){
+			
+			goalState = true;
+			winner = Math.abs(combos[y + 3])/combos[y + 3];
+			
+		}
+		if(x == y){
+			
+			combos[6] += board[x][y];
+			if(Math.abs(combos[6]) >= 3){
+				
+				goalState = true;
+				winner = Math.abs(combos[6])/combos[6];
+				
+			}
+			
+		}
+		if(x == 2 - y){
+			
+			combos[7] += board[x][y];
+			if(Math.abs(combos[7]) >= 3){
+				
+				goalState = true;
+				winner = Math.abs(combos[7])/combos[7];
+				
+			}
+			
+		}
+		
+	}
+	
+	//Getters
+	public int getMove(){
+		
+		return 1 + x + 3*y;
+				
+	}
+	
+	public boolean isGoalState(){
+		
+		return goalState;
+		
+	}
+	
+	public int getWinner(){
+		
+		return winner;
+		
+	}
+	
+	public int[][] getBoard(){
+		
+		return board;
+		
+	}
+	
+	public boolean isMyMove(){
+		
+		return isMyMove;
+		
+	}
+	
+	public boolean isBlank(int x, int y){
+		
+		return board[x][y] == 0;
+		
+	}
+	
+	public TicTacTree getParent(){
+		
+		return parent;
+		
+	}
+	
+	public TicTacTree getLeftChild(){
+		
+		return leftChild;
+		
+	}
+	
+	public TicTacTree getRightSibling(){
+		
+		return rightSibling;
+		
+	}
+	
+	//Setters
+	public void setWinner(int winner){
+		
+		this.winner = winner;
+		
+	}
+	
+	public void setLeftChild(TicTacTree leftChild){
+		
+		this.leftChild = leftChild;
+		
+	}
+	
+	public void setRightSibling(TicTacTree rightSibling){
+		
+		this.rightSibling = rightSibling;
+		
+	}
+	
+	public TicTacTree makeChild(int x, int y){
+		
+		TicTacTree child = new TicTacTree(this, board, combos, x, y, !isMyMove);
+		return child;
+		
+	}
+	
 }
--- XO.java
+++ XO.java
@@ -1,8 +1,16 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
 import java.util.Scanner;
 
 public class XO {
 
-	double[] minimaxHeap;
+
+	private TicTacTree current;
 	Scanner scanner = new Scanner(System.in);
 	//true if I am x, false if I am o
 	boolean xo;
@@ -18,13 +26,192 @@ public class XO {
 		else if(input.equals("x"))
 			xo = false;
 		else
-			System.err.println("You're a dumbass.");	
+			System.err.println("You're a dumbass.");
+		
+		if(xo)
+			current = new TicTacTree();
+		else
+			current = new TicTacTree(scanner.nextInt());
+		populateTree(current);
+		playGame();
 		
 	}
 	
-	public void minimaxConstruct(){
+	private void playGame(){
+		
+		while(!current.isGoalState()){
+		
+			TicTacTree currentChild = current.getLeftChild();
+			TicTacTree bestChild = current.getLeftChild();
+			int score = bestChild.getWinner();
+			
+			while(currentChild != null){
+				
+				if(currentChild.getWinner() > score){
+					
+					bestChild = currentChild;
+					score = bestChild.getWinner();
+					
+				}
+				currentChild = currentChild.getRightSibling();
+				
+			}
+			ArrayList<TicTacTree> bestChildren = new ArrayList<TicTacTree>();
+			currentChild = current.getLeftChild();
+			while(currentChild != null){
+				
+				if(currentChild.getWinner() == score)
+					bestChildren.add(currentChild);
+				currentChild = currentChild.getRightSibling();
+				
+			}
+			
+			Random random = new Random();
+			current = bestChildren.get(random.nextInt(bestChildren.size()));
+			printBoard(current);
+			System.out.println(current.getMove());
+			
+			if(current.isGoalState())
+				break;
+			
+			boolean goodMove = false;
+			
+			while(!goodMove){
+				
+				int move = scanner.nextInt();
+				currentChild = current.getLeftChild();
+				
+				while(currentChild != null){
+					
+					if(currentChild.getMove() == move){
+						
+						current = currentChild;
+						goodMove = true;
+						break;
+						
+					}
+					currentChild = currentChild.getRightSibling();
+				}
+				
+				if(!goodMove)
+					System.err.println("Invalid move");
+				
+			}
+			
+			printBoard(current);
+			
+		}
+		
+	}
+	
+	private void populateTree(TicTacTree current){
+
+		if(!current.isGoalState()){
+			
+			TicTacTree currentChild = null;
+			
+			for(int i = 0; i < 3; i++)
+				for(int j = 0; j < 3; j++)
+					if(current.isBlank(i, j)){
+						
+						TicTacTree child = current.makeChild(i, j);
+						if(current.getLeftChild() == null){
+							
+							current.setLeftChild(child);
+							populateTree(child);
+							current.setWinner(child.getWinner());
+							currentChild = child;
+							
+						}
+						else{
+							
+							currentChild.setRightSibling(child);
+							populateTree(child);
+							currentChild = child;
+							
+						}
+						
+						if(!current.isMyMove())
+							current.setWinner(Math.max(current.getWinner(), child.getWinner()));
+						else
+							current.setWinner(Math.min(current.getWinner(), child.getWinner()));
+						
+					}
+			
+		}
+		
+	}
+	
+	private void populateTreeAB(TicTacTree current){
 		
 		
+		
+	}
+	
+	private void printBoard(TicTacTree current){
+		
+		for(int i = 0; i < 3; i++){
+			System.err.print("[ ");
+			for(int j = 0; j < 3; j++)
+				if(current.getBoard()[j][i] == 1)
+					if(xo)
+						System.err.print("X ");
+					else
+						System.err.print("O ");
+				else if(current.getBoard()[j][i] == -1)
+					if(xo)
+						System.err.print("O ");
+					else
+						System.err.print("X ");
+				else
+					System.err.print("  ");
+			System.err.println("]");
+		}
+		System.err.println();
+		
+	}
+	
+	private void printTree() throws FileNotFoundException, UnsupportedEncodingException{
+		
+		PrintWriter writer = new PrintWriter("testdata.txt", "UTF-8");
+		
+		LinkedList<TicTacTree> queue = new LinkedList<TicTacTree>();
+		queue.add(current);
+		
+		while(!queue.isEmpty()){
+			
+			TicTacTree thisun = queue.poll();
+			
+			writer.println(thisun.getWinner() + " " + thisun.depth + " " + thisun.isMyMove());
+			for(int i = 0; i < 3; i++){
+				writer.print("[ ");
+				for(int j = 0; j < 3; j++)
+					if(thisun.getBoard()[j][i] == 1)
+						if(xo)
+							writer.print("X ");
+						else
+							writer.print("O ");
+					else if(thisun.getBoard()[j][i] == -1)
+						if(xo)
+							writer.print("O ");
+						else
+							writer.print("X ");
+					else
+						writer.print("  ");
+				writer.println("]");
+			}
+			writer.println();
+			
+			TicTacTree currentChild = thisun.getLeftChild();
+			
+			while(currentChild != null){
+				
+				queue.add(currentChild);
+				currentChild = currentChild.getRightSibling();
+				
+			}
+			
+		}
 		
 	}
 	
